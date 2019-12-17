4f7c37dbb468c66e94a811f109a67b4def2703cb
Christian <christian@megaminus.de>
2019-12-17 21:25:25 +0100

    Change to python

--- player/controller.py
+++ player/controller.py
@@ -1,6 +1,7 @@
 #Soruce:
 #https://github.com/tliero/qudio/blob/master/code/qudio.py
 
+import RPi.GPIO as GPIO
 import os
 import logging
 import time
@@ -15,31 +16,45 @@ MUSIC_BASE_DIRECTORY = "/home/pi/music/"
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s - %(message)s')
 logging.info('Initializing')
 
+PIN_LED_PHOTO = 23
+PIN_PLAY = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_LED_PHOTO, GPIO.OUT)
+GPIO.setup(PIN_PLAY, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 #function for button "play/pause"
 def play_callback(channel):
     logging.info("PLAY/PAUSE")
-    #socketIO.emit('pause')
+    cmd = "mocp -G"
+    os.system(cmd)
 
 #function for button "next song"
 def next_callback(channel):
     logging.info("NEXT")
-    #socketIO.emit('next')
+    cmd = "mocp -f"
+    os.system(cmd)
     ## TODO implement seek
 
 #function for button "previous song"
 def prev_callback(channel):
     logging.info("PREV")
-    #socketIO.emit('prev')
+    cmd = "mocp -r"
+    os.system(cmd)
     ## TODO implement jump to beginning for first x seconds (or if first track)
     ## TODO implement seek
 
 #function for button "volume up"
-def prev_callback(channel):
+def volup_callback(channel):
     logging.info("VOLUP")
+    cmd = "mocp -v +5"
+    os.system(cmd)
 
 #function for button "volume down"
-def prev_callback(channel):
+def voldown_callback(channel):
     logging.info("VOLDOWN")
+    cmd = "mocp -v -5"
+    os.system(cmd)
 
 #function for playing sounds
 def play(music_path):
@@ -54,12 +69,22 @@ def play(music_path):
     os.system(cmd)
     print("play now")
 
+
+#GPIO.add_event_detect(PIN_PREV, GPIO.FALLING, callback=prev_callback, bouncetime=400)
+GPIO.add_event_detect(PIN_PLAY, GPIO.FALLING, callback=play_callback, bouncetime=400)
+#GPIO.add_event_detect(PIN_NEXT, GPIO.FALLING, callback=next_callback, bouncetime=400)
+
+
 try:
-    #listener_thread = Thread(target=events_thread)
-    #listener_thread.daemon = True
-    #listener_thread.start()
+    listener_thread = Thread(target=events_thread)
+    listener_thread.daemon = True
+    listener_thread.start()
 
     while True:
+
+        #turn LED on for photo
+        GPIO.output(PIN_LED_PHOTO, GPIO.HIGH)
+
         #scan QR code
         zbarcam = subprocess.Popen(['zbarcam', '--quiet', '--nodisplay', '--raw', '-Sdisable', '-Sqrcode.enable', '--prescale=320x240', '/dev/video0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
@@ -80,6 +105,7 @@ try:
 
             if qr_code.startswith("cmd://"):
                 play(qr_code)
+                break
             else:
                 #replace blanks with underscore
                 qr_code.replace(" ", "_")
@@ -87,13 +113,18 @@ try:
                 full_path = MUSIC_BASE_DIRECTORY + qr_code
                 logging.debug("full_music_path: " + full_path)
                 print(full_path)
+                #play
                 play(full_path)
+                break
             
         else:
             logging.warning('Timeout on zbarcam')
             #play(SOUND_SCAN_FAIL)
 
         zbarcam.terminate()
+
+        #turn LED off for photo
+        GPIO.output(PIN_LED_PHOTO, GPIO.LOW)
 
 # Exit when Ctrl-C is pressed
 except KeyboardInterrupt:
