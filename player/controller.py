578844d452588f26cd04c3728890489d2a0d316d
Christian <christian@megaminus.de>
2020-01-02 21:58:21 +0100

    Optimize Controller Script

--- player/controller.py
+++ player/controller.py
@@ -95,8 +95,11 @@ def scan_and_play_callback(channel):
 
         if (poll_result):
             qr_code = zbarcam.stdout.readline().rstrip()
+
             logging.info(qr_code)
+            logging.info( unicode(qr_code, "utf8", errors="ignore") )
             #qr_code = qr_code.encode(encoding='UTF-8') # python3
+
             qr_code = qr_code.decode('UTF-8') # python3
             logging.info("QR Code: " + qr_code)
 
