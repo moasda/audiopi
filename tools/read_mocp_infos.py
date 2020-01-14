4567ddacc8fd0dd34375e459683aeaf6eab20643
Christian <christian@megaminus.de>
2020-01-14 21:40:13 +0100

    Optimize Controller Script

--- tools/read_mocp_infos.py
+++ tools/read_mocp_infos.py
@@ -1,3 +1,4 @@
 import subprocess
 
-print( subprocess.call(['mocp', '-i', ['|'], ['grep', 'State']], shell=False) )
+print( subprocess.call(['mocp', '-i', '|', 'grep', 'State'], shell=False) )
+#print( subprocess.call(['mocp', '-i', ['|'], ['grep', 'State']], shell=False) )
