70d6499742cc17cfb3355135ee91050f36faec93
Christian <christian@megaminus.de>
2020-01-17 15:43:13 +0100

    Timer for shudown

--- player/controller.py
+++ player/controller.py
@@ -175,7 +175,7 @@ def main():
 
     shutdown_timer_running = False
 
-    shutdown_timer = threading.Timer(300.0, shutdown_callback) #shutdown in 5 minutes
+    shutdown_timer = threading.Timer(30.0, shutdown_callback) #shutdown in 5 minutes
 
     try:
         while True:
