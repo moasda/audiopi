273ac3913566e2bd3e26b4364a1a5eedcd9eac5b
Christian <christian@megaminus.de>
2020-01-17 15:36:11 +0100

    Optimize Controller Script

--- tools/read_mocp_infos.py
+++ tools/read_mocp_infos.py
@@ -8,12 +8,17 @@ stdout,stderr = mocp_state.communicate()
 #print( stdout.split(':')[1] )
 #print( mocp_state )
 
-print( stdout )
+#print( stdout )
 
 state_play = b'PLAY' in stdout
 state_pause = b'PAUSE' in stdout
 state_stop = b'STOP' in stdout
 
-print( "Play: " + str(state_play) )
-print( "Pause: " + str(state_pause) )
-print( "Stop: " + str(state_stop) )
+if state_play == True:
+    print( "Play: " + str(state_play) )
+
+if state_pause == True:
+    print( "Pause: " + str(state_pause) )
+
+if state_stop == True:
+    print( "Stop: " + str(state_stop) )
