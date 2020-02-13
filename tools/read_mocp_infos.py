7de300f863051279e863d011e8cb545ba7284e2f
Christian (AudioPI) <christian@megaminus.de>
2020-02-13 21:57:04 +0000

    Read mocp infos

--- tools/read_mocp_infos.py
+++ tools/read_mocp_infos.py
@@ -1,19 +1,33 @@
 import subprocess
 
 #Read current file
-mocp_file = subprocess.Popen(['mocp', '-i', '|', 'grep', 'File'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
-stdout,stderr = mocp_file.communicate()
+mocp_info = subprocess.Popen(['mocp', '-i'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
+
+for line in mocp_info.stdout:
+  line = line.decode('utf-8')
+  if line.startswith('File') == True:
+    print( line )
+    break
+#line = mocp_file.stdout.readline()
+
+
+#print( str(test) )
+#print( '\n\n' )
+
+stdpit.stderr = mocp_info.communicate()
+
 #Decode from binary string without prefix "File: "(6) and suffix "\n"
 current_track = stdout[6:-1].decode('utf-8')
 
-print( "Aktuell: " + current_track )
-print( "Vergleich: " + title )
+print( "Aktuell: " + str(current_track) )
+#print( "Vergleich: " + title )
 
 
-exit(-1)
+print("###############################")
+#exit(-1)
 
 #mocp_state = str( subprocess.call(['mocp', '-i', '|', 'grep', 'State'], shell=False) )
-mocp_state = subprocess.Popen(['mocp', '-i', '|', 'grep', 'State'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+mocp_state = subprocess.Popen(['mocp', '-i', '|', 'grep', 'File'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 stdout,stderr = mocp_state.communicate()
 
